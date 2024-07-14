72f4f002b477919cb7f549d07a5551e95a8f4799
leone-nyaga <leonenyaga@gmail.com>
2024-07-14 05:58:31 -0700

    ES6 CLASSES

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,4 @@
-import Classroom from './0-classroom.js';
+import Classroom from './0-classroom';
 
 export default function initializeRooms() {
   return [19, 20, 34].map((maxStudentSize) => new Classroom(maxStudentSize));
