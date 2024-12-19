9f9235a5e58bbd8d410bef0e1b0b059ed081b09e
leone-nyaga <leonenyaga@gmail.com>
2024-12-19 13:06:13 +0300

    fixed error on Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdout.write('Welcome to ALX, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
-    process.stdout.write('Your name is: ${name}');
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 process.stdin.on('end', () => {
