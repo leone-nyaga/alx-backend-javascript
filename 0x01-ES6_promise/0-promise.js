bcc32db453066d8d824dd7b3fbbb98473d485d80
leone-nyaga <leonenyaga@gmail.com>
2025-11-03 14:17:07 +0300

    Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // eslint-disable-next-line no-unused-vars
+  return new Promise((resolve) => {
     resolve('Response received');
   });
 }
