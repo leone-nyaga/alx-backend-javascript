d07b7a1ed6d0947f6a7a21fae1056107eeb4e8af
leone-nyaga <leonenyaga@gmail.com>
2025-11-26 21:37:09 +0300

    Syntax correction

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class ALXCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
